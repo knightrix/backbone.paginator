2035d793c8893660175999ed0e814b7935963dff
Pyie <xvariablex@gmail.com>
2012-11-02 16:36:40 -0400

    pagination start at 1 instead of 0 and added url

--- examples/netflix-request-paging/collections/PaginatedCollection.js
+++ examples/netflix-request-paging/collections/PaginatedCollection.js
@@ -35,14 +35,14 @@
 		
 		paginator_ui: {
 			// the lowest page index your API allows to be accessed
-			firstPage: 0,
+			firstPage: 1,
 		
 			// which page should the paginator start from 
 			// (also, the actual page the paginator is on)
-			currentPage: 0,
+			currentPage: 1,
 			
 			// how many items per page should be shown
-			perPage: 3,
+			perPage: 4,
 			
 			// a default number of total pages to query in case the API or 
 			// service you are using does not support providing the total 
